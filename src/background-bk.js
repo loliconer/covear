3361308d27f16d7db76bf93fafe923c722747c65
loliconer <vuedev@163.com>
2019-04-26 17:50:17 +0800

    Update

--- src/background-bk.js
+++ src/background-bk.js
@@ -4,15 +4,19 @@ import {
   createProtocol,
   installVueDevtools
 } from 'vue-cli-plugin-electron-builder/lib'
-
+// import Launcher from './Launcher'
 import ConfigManager from './main/core/ConfigManager'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 is.windows() && app.setAppUserModelId('dapp.lovue.covear')
+// Standard scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([{ scheme: 'app', secure: true }])
 
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
 let win
+
 function createWindow() {
   win = new BrowserWindow({
     title: '掩耳',
@@ -25,17 +29,35 @@ function createWindow() {
   // win.setMenu(null)
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     win.loadURL('app://./index.html')
   }
+
   win.on('closed', () => win = null)
 }
 
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) createWindow()
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
+    // Install Vue Devtools
     try {
       await installVueDevtools()
     } catch (e) {
@@ -58,14 +80,7 @@ app.on('ready', async () => {
   createWindow()
 })
 
-app.on('activate', () => {
-  if (win === null) createWindow()
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
-
+// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', data => {
