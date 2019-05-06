3378231ae8f949a855f8e41cb8b63a7f7bee2603
loliconer <vuedev@163.com>
2019-05-07 00:07:57 +0800

    improve: Implementing client for aria2

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,12 +1,18 @@
 import './less/style.less'
-import {$fetch} from 'lovue/dist/utils.esm'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {remote} from 'electron'
+import Client from './js/aria2'
+
+const configManager = remote.getGlobal('configManager')
 
 Vue.config.productionTip = false
 
-window.$fetch = $fetch
+window.client = new Client({
+  port: configManager.getSystemConfig('rpc-listen-port'),
+  secret: configManager.getSystemConfig('rpc-secret')
+})
 
 new Vue({
   router,
--- src/renderer/js/aria2.js
+++ src/renderer/js/aria2.js
@@ -1,9 +1,22 @@
 import EventEmitter from 'events'
-import {remote} from 'electron'
 
-const configManager = remote.getGlobal('configManager')
+function prefix(method) {
+  if (!method.startsWith('system.') && !method.startsWith('aria2.'))
+    method = `aria2.${method}`
 
-class Client extends EventEmitter {
+  return method
+}
+
+class Deferred {
+  constructor() {
+    this.promise = new Promise((resolve, reject) => {
+      this.resolve = resolve
+      this.reject = reject
+    })
+  }
+}
+
+export default class Client extends EventEmitter {
   constructor(options = {}) {
     super()
 
@@ -14,6 +27,7 @@ class Client extends EventEmitter {
     this.path = options.path || '/jsonrpc'
     this.ws = null
     this.lastId = 0
+    this.defers = {}
 
     init()
   }
@@ -28,35 +42,71 @@ class Client extends EventEmitter {
     ws.onerror = event => this.emit('error', event)
     ws.onmessage = event => {
       const message = JSON.parse(event.data)
-      this.emit('receive', message)
+
+      if (Array.isArray(message)) {
+        for (const row of message) this.handleMsg(row)
+      } else {
+        this.handleMsg(message)
+      }
     }
 
     this.ws = ws
+  }
+
+  handleMsg(message) {
+    if (message.method === undefined) {
+      const {id, error, result} = message
+      const defer = this.defers[id]
+
+      if (!defer) return
+
+      if (error) return defer.reject(error)
+
+      defer.resolve(result)
+      delete this.defers[id]
+    } else if (message.id === undefined) {
+      this.emit(message.method, message.params)
+    }
   }
 
   id() {
     return this.lastId++
   }
 
-  send(method, ...params) {
-    if (this.ws.readyState !== 1) return
-
-    if (!method.startsWith('system.') && !method.startsWith('aria2.'))
-      method = `aria2.${method}`
-
+  secretParams(params) {
     if (Array.isArray(params)) {
       if (this.secret) params.unshift(`token:${this.secret}`)
     } else {
       if (this.secret) params = [`token:${this.secret}`]
     }
+    return params
+  }
+
+  send(method, ...params) {
+    if (this.ws.readyState !== 1) return
+
+    params = this.secretParams(params)
 
     const message = {
-      method,
+      method: prefix(method),
       'json-rpc': '2.0',
       id: this.id()
     }
     if (params) message.params = params
     this.ws.send(JSON.stringify(message))
+
+    const defer = new Deferred()
+    this.defers[message.id] = defer
+    return defer.promise
+  }
+
+  async multi(calls) {
+    return this.send('system.multicall', [
+      calls.map(([method, ...params]) => ({
+        methodName: prefix(method),
+        params: this.secretParams(params)
+      }))
+    ])
   }
 
   close() {
