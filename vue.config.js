509e29474a5eed6bc65aa843fcb7ea1465698fb6
loliconer <vuedev@163.com>
2019-08-26 19:14:39 +0800

    Add build options

--- vue.config.js
+++ vue.config.js
@@ -54,5 +54,53 @@ module.exports = {
     plugins: process.env.NODE_ENV === 'production' ? [
       new ReplaceVendorsPlugin()
     ] : []
+  },
+  pluginOptions: {
+    electronBuilder: {
+      mainProcessFile: 'src/background.js',
+      builderOptions: {
+        productName: 'Covear',
+        appId: 'dapp.lovue.covear',
+        directories: {
+          output: 'release'
+        },
+        dmg: {
+          window: {
+            width: 540,
+            height: 380
+          },
+          contents: [{
+            x: 410,
+            y: 230,
+            type: 'link',
+            path: '/Application'
+          }, {
+            x: 130,
+            y: 230,
+            type: 'file'
+          }]
+        },
+        win: {
+          target: [{
+            target: 'nsis',
+            arch: ['x64']
+          }, {
+            target: 'zip',
+            arch: ['x64']
+          }, {
+            target: 'portable',
+            arch: ['x64']
+          }]
+        },
+        nsis: {
+          oneClick: false,
+          allowToChangeInstallationDirectory: true
+        },
+        linux: {
+          category: 'Network',
+          target: ['deb', 'snap', 'AppImage']
+        }
+      }
+    }
   }
 }
